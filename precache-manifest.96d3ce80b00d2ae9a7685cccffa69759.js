234ec3bd49b76345c05dbc40e4e15732d996a173
diegocamy <53247032+diegocamy@users.noreply.github.com>
2020-01-04 19:48:01 -0300

    Updates

---
+++ precache-manifest.96d3ce80b00d2ae9a7685cccffa69759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586b6001b285a137e4c384c5bf1bf21a",
+    "url": "/escribeYA/index.html"
+  },
+  {
+    "revision": "b3f4b28c8e9417fb54d9",
+    "url": "/escribeYA/static/css/main.a31f63bf.chunk.css"
+  },
+  {
+    "revision": "9eda78e4ffe4882bdbee",
+    "url": "/escribeYA/static/js/2.fe64193a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/escribeYA/static/js/2.fe64193a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3f4b28c8e9417fb54d9",
+    "url": "/escribeYA/static/js/main.3664153b.chunk.js"
+  },
+  {
+    "revision": "0521e7c963ea5a480aae",
+    "url": "/escribeYA/static/js/runtime-main.e19cf794.js"
+  },
+  {
+    "revision": "0654a0112ebef09a6b882d34789566a4",
+    "url": "/escribeYA/static/media/bg.0654a011.svg"
+  }
+]);
